418584c1da9f19dedeaac3a645db8c456cb23d8a
Jaron Matzinger <jaron.matzinger@purpur.ventures>
2025-04-10 17:58:11 +0200

    worked on mlserver

---
+++ lab07/tinyvit/inference_client.py
@@ -0,0 +1,17 @@
+import requests
+import numpy as np
+
+from PIL import Image
+
+from mlserver.codecs import NumpyCodec
+from mlserver.types import InferenceRequest
+
+inference_url = 'http://localhost:8080/v2/models/tinyvit/infer'
+input_data = np.array(Image.open('../imgs/cat.jpg'))
+
+inference_request = InferenceRequest(
+    inputs=[NumpyCodec.encode_input(name='payload', payload=input_data)]
+)
+
+res = requests.post(inference_url, json=inference_request.dict())
+print(res.json()['outputs'][0]['data'])
--- lab07/tinyvit/serve-model.py
+++ lab07/tinyvit/serve-model.py
@@ -1,14 +1,25 @@
+import os
+import json
+import timm
+import torch
+import numpy as np
+
+from PIL import Image
+
 from mlserver import MLModel
 from mlserver.codecs import decode_args
-import numpy as np
-from PIL import Image
-import timm
-
 
 class TinyViTModel(MLModel):
 
+    def __init__(self, *args, **kwargs):
+        super(TinyViTModel, self).__init__(*args, **kwargs)
+        dirname = os.path.dirname(__file__)
+        with open(f'{dirname}/imagenet-22k-id2label.json') as f:
+            d = json.load(f)
+        self.label_dict = d
+
     async def load(self) -> bool:
-        self._model = timm.create_model("tiny_vit_5m_224.dist_in22k", pretrained=True)
+        self._model = timm.create_model('tiny_vit_5m_224.dist_in22k', pretrained=True)
         self._model.eval()
 
         data_config = timm.data.resolve_data_config({}, model=self._model)
@@ -21,5 +32,13 @@ class TinyViTModel(MLModel):
         img = Image.fromarray(payload)
         img = self._transforms(img).unsqueeze(0)
         output = self._model(img)
-        output = output.softmax(dim=1)
-        return output.detach().numpy()
+        _, top5_class_indices = torch.topk(output.softmax(dim=1) * 100, k=5)
+        #top5_classes = [self.label_dict[str(idx)] for idx in top5_class_indices.squeeze().tolist()]
+        top5_classes = [
+            self.label_dict[str(idx)].split(',')[0].strip()
+            for idx in top5_class_indices.squeeze().tolist()
+        ]
+        return np.array(top5_classes)
+    
+        #output = output.softmax(dim=1)
+        #return output.detach().numpy()
